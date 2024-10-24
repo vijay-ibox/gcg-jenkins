1082416de3037c76f0e311ffe39812aac40a969a
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-10-24 12:34:04 +0530

    Create EmailAPI.groovy

---
+++ src/org/fox/jenkins/EmailAPI.groovy
@@ -0,0 +1,21 @@
+// EmailAPI.groovy
+ // Replace with your desired package
+package org.fox.jenkins
+
+class EmailAPI {
+
+    String sendEmail() {
+        // Your email sending logic here using JavaMail or another library
+        return "Sending email to"
+    }
+}
+
+
+// In your main Groovy file:
+
+//class MyProgram {
+//    static void main(String[] args) {
+//        def emailAPI = new EmailAPI()
+//        emailAPI.sendEmail()
+//    }
+//}
