140332942dd6962e511fe68aee790b986675418a
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-10-25 20:03:14 +0530

    Update EmailAPI.groovy

--- src/org/fox/jenkins/EmailAPI.groovy
+++ src/org/fox/jenkins/EmailAPI.groovy
@@ -41,16 +41,18 @@ class EmailAPI {
     }
 
     String triggerApi(String apiUrl, String value1) {
-        String data_to_send = "{\n" +
-                "   \"name\": value1,\n" +
-                "   \"data\": {\n" +
-                "      \"year\": 2019,\n" +
-                "      \"price\": 1849.99,\n" +
-                "      \"CPU model\": \"Intel Core i9\",\n" +
-                "      \"Hard disk size\": \"1 TB\"\n" +
-                "   }\n" +
-                "}";
-
+//        String data_to_send = "{\n" +
+//                "   \"name\": " + value1 + ","
+//                "   \"data\": {\n" +
+//                "      \"year\": 2019,\n" +
+//                "      \"price\": 1849.99,\n" +
+//                "      \"CPU model\": \"Intel Core i9\",\n" +
+//                "      \"Hard disk size\": \"1 TB\"\n" +
+//                "   }\n" +
+//                "}";
+        Map<String, String> mapValue = new HashMap<>();
+        mapValue.put("name",value1);
+//        JsonHelper.convertToJsonStringFromMap(mapValue);
         URL url = new URL(apiUrl)
         HttpURLConnection connection = (HttpURLConnection) url.openConnection()
         connection.setRequestMethod("POST")
@@ -59,7 +61,7 @@ class EmailAPI {
 //        connection.setRequestProperty("body",data_to_send)
         try {
             DataOutputStream os = new DataOutputStream(connection.getOutputStream())
-            os.writeBytes(data.toString());
+            os.writeBytes(JsonHelper.convertToJsonStringFromMap(mapValue));
             os.flush();
         } catch (e) {
             println(e)
