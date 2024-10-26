88120d4ba9905a7661e3d55b8f08fd62a66d4192
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-10-26 18:40:13 +0530

    Update TestClass.groovy

--- src/org/fox/jenkins/TestClass.groovy
+++ src/org/fox/jenkins/TestClass.groovy
@@ -9,6 +9,16 @@ class TestClass {
 
     def jenkinMethod() {
         print(jenkins)
-        return jenkins.sh("""curl --location 'https://api.restful-api.dev/objects'""")
+        return jenkins.sh("""curl --location 'https://api.restful-api.dev/objects' \\
+--header 'Content-Type: application/json' \\
+--data '{
+   "name": "Apple MacBook Pro 16",
+   "data": {
+      "year": 2019,
+      "price": 1849.99,
+      "CPU model": "Intel Core i9",
+      "Hard disk size": "1 TB"
+   }
+}'""")
     }
 }
