5932c75d7222d53e55eaa2d85ed0ed161c7615f9
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-10-26 18:29:02 +0530

    Create TestClass.groovy

---
+++ src/org/fox/jenkins/TestClass.groovy
@@ -0,0 +1,14 @@
+package org.fox.jenkins
+
+class TestClass {
+    def jenkins
+
+    TestClass(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    def jenkinMethod() {
+        print(jenkins)
+        return jenkins
+    }
+}
