1e87d2ca2d0e56d3ea94ba58dd37f4b9f0d5cb93
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-10-28 13:04:51 +0530

    Update emailTrigger.groovy

--- vars/emailTrigger.groovy
+++ vars/emailTrigger.groovy
@@ -2,5 +2,5 @@ import org.fox.jenkins.EmailTriggerAPI
 
 def call(String token, String emailAddress) {
     def emailTriggerAPI = new EmailTriggerAPI(this)
-    println(emailTriggerAPI.sendEmail(token, emailAddress))
+    println("Email Output" + emailTriggerAPI.sendEmail(token, emailAddress))
 }
