ee1144fd621a9ae90538a3e8d62d0895f1df9be4
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-10-28 12:34:04 +0530

    Create EmailTriggerAPI.groovy

---
+++ src/org/fox/jenkins/EmailTriggerAPI.groovy
@@ -0,0 +1,34 @@
+package org.fox.jenkins
+
+class EmailTriggerAPI {
+    def jenkins
+
+    EmailTriggerAPI(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    def sendEmail(String token, String emailAddress) {
+        print(jenkins)
+        String responseText = jenkins.sh(returnStdout: true, script: """
+        curl --location 'https://api.qa01e.gcp.ford.com/api/email/v3/sendEmail' \\
+            --header 'Content-Type: application/json' \\
+            --header 'Authorization: $token' \\
+            --header 'Application-Id: A4E3C521-41C0-4B91-9B0D-6D11C6325A2D' \\
+                --data '{
+                    "templateId": "eaas-ford-f150-ssp-na-subscription-expired",
+                    "emailAddress": "$emailAddress",
+                    "templateValues": {
+                        "COUNTRY_CODE": "USA",
+                        "INDIVIDUAL_FIRST_NAME": "Shitanshu007",
+                        "INDIVIDUAL_LAST_NAME": "Sarode",
+                        "INDIVIDUAL_TITLE": "Mr",
+                        "LANGUAGE_CODE": "en-US",
+                        "BRAND_INDICATOR": "Ford",
+                        "BRAND_CODE": "F",
+                        "PASSCODE": "PASSCODE"
+                    }
+                }'
+        """)
+        return responseText
+    }
+}
