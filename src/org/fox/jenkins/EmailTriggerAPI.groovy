44d32a600ff552687b079fd95f0b89960d8ae365
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-10-28 13:52:47 +0530

    Update EmailTriggerAPI.groovy

--- src/org/fox/jenkins/EmailTriggerAPI.groovy
+++ src/org/fox/jenkins/EmailTriggerAPI.groovy
@@ -8,32 +8,40 @@ class EmailTriggerAPI {
     }
 
     def sendEmail(String token, String emailAddress) {
-        print(jenkins)
-        def responseText = jenkins.sh(returnStdout: true, script: """
-        curl --location 'https://api.qa01e.gcp.ford.com/api/email/v3/sendEmail' \\
-            --header 'Content-Type: application/json' \\
-            --header 'Authorization: $token' \\
-            --header 'Application-Id: A4E3C521-41C0-4B91-9B0D-6D11C6325A2D' \\
-                --data '{
-                    "templateId": "eaas-ford-f150-ssp-na-subscription-expired",
-                    "emailAddress": "$emailAddress",
-                    "templateValues": {
-                        "COUNTRY_CODE": "USA",
-                        "INDIVIDUAL_FIRST_NAME": "Shitanshu007",
-                        "INDIVIDUAL_LAST_NAME": "Sarode",
-                        "INDIVIDUAL_TITLE": "Mr",
-                        "LANGUAGE_CODE": "en-US",
-                        "BRAND_INDICATOR": "Ford",
-                        "BRAND_CODE": "F",
-                        "PASSCODE": "PASSCODE"
-                    }
-                }'
-        """)
-        echo responseText
-        jenkins.println("Response1" + response)
-        def response = JsonHelper.parse(responseText)
-        print("Response" + response)
-        jenkins.println("Response1" + response)
-        return responseText
+        Map<String, Object> mapValue = new HashMap<>();
+        mapValue.put("templateId","eaas-ford-f150-ssp-na-subscription-expired");
+        mapValue.put("emailAddress",emailAddress);
+        Map<String, String> mapValue1 = new HashMap<>();
+        mapValue1.put("COUNTRY_CODE","USA");
+        mapValue1.put("LANGUAGE_CODE","en-US");
+        mapValue1.put("INDIVIDUAL_FIRST_NAME","TestUser");
+        mapValue1.put("INDIVIDUAL_LAST_NAME","Test");
+        mapValue1.put("PASSCODE","PASSCODE");
+        mapValue.put("templateValues", mapValue1);
+        def response = openPullRequest("https://api.qa01e.gcp.ford.com/api/email/v3/sendEmail", mapValue, token)
+        return response
+    }
+
+    def openPullRequest(requestUrl, body, token) {
+        def restHub = new RestHub()
+        def data = makePostRequest(requestUrl, body, token)
+        def message = restHub.parseResponse(data)
+        def statusCode = restHub.returnStatusCode(data)
+        return [statusCode, message]
+    }
+
+    def makePostRequest(requestUrl, body, token) {
+        def headers = createHeaders(token)
+//        headers['Content-Type'] = 'application/json'
+        def response = new RestHub().doPostHttpRequestWithJson(requestUrl, headers, body)
+        return response
+    }
+
+    def createHeaders(token) {
+        def headers = [:]
+        headers['Content-Type'] = "application/json"
+        headers['Authorization'] = token
+        headers['Application-Id'] = "A4E3C521-41C0-4B91-9B0D-6D11C6325A2D"
+        return headers
     }
 }
