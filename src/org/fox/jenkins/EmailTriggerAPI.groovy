3150d481205a5a42a0a97277133d6fc6734fdf53
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-10-28 16:25:14 +0530

    Update EmailTriggerAPI.groovy

--- src/org/fox/jenkins/EmailTriggerAPI.groovy
+++ src/org/fox/jenkins/EmailTriggerAPI.groovy
@@ -8,6 +8,22 @@ class EmailTriggerAPI {
     }
 
     def sendEmail(String token, String emailAddress) {
+//        Map<String, Object> mapValue = new HashMap<>();
+//        mapValue.put("templateId","eaas-ford-f150-ssp-na-subscription-expired");
+//        mapValue.put("emailAddress",emailAddress);
+//        Map<String, String> mapValue1 = new HashMap<>();
+//        mapValue1.put("COUNTRY_CODE","USA");
+//        mapValue1.put("LANGUAGE_CODE","en-US");
+//        mapValue1.put("INDIVIDUAL_FIRST_NAME","TestUser");
+//        mapValue1.put("INDIVIDUAL_LAST_NAME","Test");
+//        mapValue1.put("PASSCODE","PASSCODE");
+//        mapValue.put("templateValues", mapValue1);
+//        def body = JsonHelper.convertToJsonStringFromMap(mapValue)
+        def response = triggerApi("https://api.qa01e.gcp.ford.com/api/email/v3/sendEmail", emailAddress, token)
+        return response
+    }
+
+    static String triggerApi(String apiUrl, String emailAddress, String token) {
         Map<String, Object> mapValue = new HashMap<>();
         mapValue.put("templateId","eaas-ford-f150-ssp-na-subscription-expired");
         mapValue.put("emailAddress",emailAddress);
@@ -19,8 +35,35 @@ class EmailTriggerAPI {
         mapValue1.put("PASSCODE","PASSCODE");
         mapValue.put("templateValues", mapValue1);
         def body = JsonHelper.convertToJsonStringFromMap(mapValue)
-        def response = openPullRequest("https://api.qa01e.gcp.ford.com/api/email/v3/sendEmail", body, token)
-        return response
+        URL url = new URL(apiUrl)
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection()
+        connection.setRequestMethod("POST")
+        connection.setDoOutput(true)
+        connection.setRequestProperty("Content-Type", "application/json")
+        connection.setRequestProperty("Authorization", token)
+        connection.setRequestProperty("Application-Id", "A4E3C521-41C0-4B91-9B0D-6D11C6325A2D")
+        try {
+            DataOutputStream os = new DataOutputStream(connection.getOutputStream())
+            os.writeBytes(body);
+            os.flush();
+        } catch (e) {
+            println(e)
+        }
+        int responseCode = connection.getResponseCode()
+        println("Response Code: " + responseCode)
+        if (responseCode > 100) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()))
+            String inputLine
+            StringBuffer response = new StringBuffer()
+            while ((inputLine = bufferedReader.readLine()) != null) {
+                response.append(inputLine)
+            }
+            bufferedReader.close()
+            // Print the response
+            return ("Response: " + response.toString())
+        } else {
+            return ("GET request failed")
+        }
     }
 
     def openPullRequest(requestUrl, body, token) {
