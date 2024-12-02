a833ad022bfa62233b9e9acf2453fc4cc0f5c239
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-12-02 11:46:02 +0530

    Update listenEmail.groovy

--- vars/listenEmail.groovy
+++ vars/listenEmail.groovy
@@ -2,10 +2,13 @@ import org.fox.jenkins.GmailAndGroovy
 
 def call() {
     def gmailAndGroovy = new GmailAndGroovy(this)
-//    println(gmailAndGroovy.readEmail().length())
-//    if(gmailAndGroovy.readEmail().length() > 0) {
+    StringBuilder builder = new StringBuilder()
+    builder.append(gmailAndGroovy.readEmail())
+    println(builder.length())
+    println(builder.length)
+    if(gmailAndGroovy.readEmail().length() > 0) {
         println("Response : " + gmailAndGroovy.readEmail())
-//        return "true"
-//    }
+        return "true"
+    }
     return "false"
 }
