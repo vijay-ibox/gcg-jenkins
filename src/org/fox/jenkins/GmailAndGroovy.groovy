c7aee2d280039efa913213085fcc6fe9f5d960ed
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-12-02 10:38:52 +0530

    Update GmailAndGroovy.groovy

--- src/org/fox/jenkins/GmailAndGroovy.groovy
+++ src/org/fox/jenkins/GmailAndGroovy.groovy
@@ -26,16 +26,16 @@ class GmailAndGroovy {
         props.setProperty("mail.imap.ssl.enable", "true");
         def responseValue = new HashMap<>();
         try {
-        def session = Session.getDefaultInstance(props);
-        def store = session.getStore("imaps")
+            def session = Session.getDefaultInstance(props);
+            def store = session.getStore("imaps")
 
-        store.connect(host, username, password)
-        def folder = store.getFolder("INBOX")
+            store.connect(host, username, password)
+            def folder = store.getFolder("INBOX")
 
-        folder.open(Folder.READ_WRITE)
+            folder.open(Folder.READ_WRITE)
 
-        Flags seen = new Flags(Flags.Flag.SEEN);
-        FlagTerm unseenFlagTerm = new FlagTerm(seen, false);
+            Flags seen = new Flags(Flags.Flag.SEEN);
+            FlagTerm unseenFlagTerm = new FlagTerm(seen, false);
             SearchTerm searchTermFlag = new FlagTerm(new Flags(Flags.Flag.SEEN), false);
             SearchTerm searchTermSubject = new SubjectTerm("Your access to Connected Services has expired");
 //            SearchTerm[] arr = new SearchTerm[]{searchTermFlag, searchTermSubject};
@@ -44,20 +44,19 @@ class GmailAndGroovy {
             SearchTerm searchTerm = new AndTerm(arr);
 
 
-        Message[] msgValue = folder.search(searchTerm);
-        FetchProfile fetchProfile = new FetchProfile()
-        fetchProfile.add(FetchProfile.Item.ENVELOPE)
-        folder.fetch(msgValue,fetchProfile)
-        for ( msgVal in msgValue ) {
-            responseValue.put("receivedDate : " , msgVal.getReceivedDate().toString())
-            responseValue.put("from : " , msgVal.getFrom()[0].toString())
-            responseValue.put("subject : " , msgVal.getSubject().toString())
-            msgVal.setFlag(Flags.Flag.SEEN, true)
-        }
+            Message[] msgValue = folder.search(searchTerm);
+            FetchProfile fetchProfile = new FetchProfile()
+            fetchProfile.add(FetchProfile.Item.ENVELOPE)
+            folder.fetch(msgValue,fetchProfile)
+            for ( msgVal in msgValue ) {
+                responseValue.put("receivedDate : " , msgVal.getReceivedDate().toString())
+                responseValue.put("from : " , msgVal.getFrom()[0].toString())
+                responseValue.put("subject : " , msgVal.getSubject().toString())
+                msgVal.setFlag(Flags.Flag.SEEN, true)
+            }
             return "Success output" + responseValue.toString()
         } catch (e) {
             println(e.message)
-            listValue.add(e.message)
             return "Failed output : " + responseValue.toString()
         }
     }
