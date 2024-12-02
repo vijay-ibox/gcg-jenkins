a821cb6404b583ed810a88cbc995115a83af6476
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-12-02 17:14:12 +0530

    Update emailTestAPI.groovy

--- vars/emailTestAPI.groovy
+++ vars/emailTestAPI.groovy
@@ -3,5 +3,5 @@ import org.fox.jenkins.EmailTestAPI
 def call(String requestUrl, String token, String emailAddress, String countryCode, String languageCode) {
     def emailTestAPI = new EmailTestAPI()
     def response = emailTestAPI.sendTestEmail(requestUrl, token, emailAddress, countryCode, languageCode)
-    println("$response.statusCode")
+    println(response.statusCode)
 }
