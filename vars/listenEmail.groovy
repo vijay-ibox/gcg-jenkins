53e13d240f6dd57159d0d6d1ea09c1575e6a1456
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-12-02 19:34:28 +0530

    Update listenEmail.groovy

--- vars/listenEmail.groovy
+++ vars/listenEmail.groovy
@@ -3,8 +3,8 @@ import org.fox.jenkins.GmailAndGroovy
 def call() {
     def gmailAndGroovy = new GmailAndGroovy(this)
     def responseOutput = gmailAndGroovy.readEmail()
-    def valLength = responseOutput.length()
     println("Response : ${responseOutput}")
+    def valLength = responseOutput.length()
     println("Response length: ${valLength}")
     if(valLength > 2) {
         return "true"
