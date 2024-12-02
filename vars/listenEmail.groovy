5fe53f827862461ed202ebb160d1edabe6fd3f23
vijay-ibox <93829633+vijay-ibox@users.noreply.github.com>
2024-12-02 11:52:23 +0530

    Update listenEmail.groovy

--- vars/listenEmail.groovy
+++ vars/listenEmail.groovy
@@ -4,8 +4,8 @@ def call() {
     def gmailAndGroovy = new GmailAndGroovy(this)
     StringBuilder builder = new StringBuilder()
     builder.append(gmailAndGroovy.readEmail())
-    println(builder.length())
-    println(builder.length)
+//    println(builder.length())
+//    println(builder.length)
     if(gmailAndGroovy.readEmail().length() > 0) {
         println("Response : " + gmailAndGroovy.readEmail())
         return "true"
